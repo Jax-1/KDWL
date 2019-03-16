ddfe1181bff43f55cc0b1bf97b7af6fd4e878aeb
Zhang <348008240@qq.com>
2019-03-16 20:33:06 +0800

    page

---
+++ kdwl/src/main/java/com/kdwl/IndexController.java
@@ -0,0 +1,22 @@
+package com.kdwl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class IndexController {
+
+    @RequestMapping("")
+    public String toIndex(Model model){
+        model.addAttribute("page","kdwl/main");
+        return "index";
+    }
+    @RequestMapping("/{page}")
+    public String toPage(Model model, @PathVariable String page){
+        model.addAttribute("page","kdwl/"+page);
+       return "index";
+    }
+}
---
+++ kdwl/src/main/resources/static/js/rolling.js
@@ -0,0 +1,19 @@
+var speed=30;
+
+var tabLeft=document.getElementById("demoLeft");
+var tab1=document.getElementById("demo1");
+var tab2=document.getElementById("demo2");
+tab2.innerHTML=tab1.innerHTML;
+
+function Marquee1(){
+  if(tabLeft.scrollLeft>=tab2.offsetWidth)
+    tabLeft.scrollLeft-=tab1.offsetWidth
+  else{
+    tabLeft.scrollLeft++;
+  }
+}
+
+var MyMar=setInterval(Marquee1,speed);
+
+tabLeft.onmouseover=function() {clearInterval(MyMar)};
+tabLeft.onmouseout=function() {MyMar=setInterval(Marquee1,speed)};
