398e7be36e8439377370253baee92e3948b2c5e1
Zhang <348008240@qq.com>
2019-04-03 20:00:31 +0800

    config

--- auth-group/auth-service/src/main/java/com/app/auth/config/SecurityConfiguration.java
+++ auth-group/auth-service/src/main/java/com/app/auth/config/SecurityConfiguration.java
@@ -1,10 +1,15 @@
 package com.app.auth.config;
 
+import com.app.auth.dto.AuthLoginConfig;
+import com.netflix.ribbon.proxy.annotation.Http;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 
 /**
  * @Class SecurityConfiguration
@@ -15,6 +20,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  **/
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthLoginConfig authLoginConfig;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         super.configure(auth);
@@ -22,22 +31,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/app/hello","/login.html").permitAll()
-                .antMatchers("/app/h1").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
+
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry expressionInterceptUrlRegistry=http.authorizeRequests();
+        //拦截过滤配置
+        for (String url:authLoginConfig.getAntMatchers()){
+            expressionInterceptUrlRegistry.antMatchers(url).permitAll();
+        }
+        expressionInterceptUrlRegistry.anyRequest().authenticated().
+                and().formLogin()
                 //指定登录页的路径
-                .loginPage("/app/login")
+                .loginPage(authLoginConfig.getLoginPage())
                 //指定自定义form表单请求的路径
-                .loginProcessingUrl("/authentication/form")
+                .loginProcessingUrl(authLoginConfig.getFormRest())
                 .failureUrl("/login?error")
                 .defaultSuccessUrl("/success")
                 //必须允许所有用户访问我们的登录页（例如未验证的用户，否则验证流程就会进入死循环）
                 //这个formLogin().permitAll()方法允许所有用户基于表单登录访问/login这个page。
                 .permitAll();
+
         //默认都会产生一个hiden标签 里面有安全相关的验证 防止请求伪造 这边我们暂时不需要 可禁用掉
         http .csrf().disable();
 
@@ -45,6 +56,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
+
         super.configure(web);
+        //资源目录拦截处理
+//        for (String url:authLoginConfig.getResources()){
+//            web.ignoring().antMatchers(url);
+//        }
+
     }
 }
--- auth-group/auth-service/src/main/java/com/app/auth/dto/AuthLoginConfig.java
+++ auth-group/auth-service/src/main/java/com/app/auth/dto/AuthLoginConfig.java
@@ -1,22 +1,67 @@
-//package com.app.auth.dto;
-//
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * @Class AuthLoginConfig
-// * @Author zhangjie
-// * @Description //TODO
-// * @Date 2019/4/1 16:35
-// * @Version 1.0
-// **/
-//@Component
-//@ConfigurationProperties(prefix = "security")
-//public class AuthLoginConfig {
-//    private String loginPage;
-//    //form表单请求路径
-//    private String formRest;
-//
-//    private String antMatchers;
-//
-//}
+package com.app.auth.dto;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @Class AuthLoginConfig
+ * @Author zhangjie
+ * @Description //TODO
+ * @Date 2019/4/1 16:35
+ * @Version 1.0
+ **/
+@Component
+@ConfigurationProperties(prefix = "security")
+public class AuthLoginConfig {
+    private String loginPage;
+    //form表单请求路径
+    private String formRest;
+
+    private List<String> antMatchers;
+
+    private List<String> resources;
+
+    public String getLoginPage() {
+        return loginPage;
+    }
+
+    public void setLoginPage(String loginPage) {
+        this.loginPage = loginPage;
+    }
+
+    public String getFormRest() {
+        return formRest;
+    }
+
+    public void setFormRest(String formRest) {
+        this.formRest = formRest;
+    }
+
+    public List<String> getAntMatchers() {
+        return antMatchers;
+    }
+
+    public void setAntMatchers(List<String> antMatchers) {
+        this.antMatchers = antMatchers;
+    }
+
+    public List<String> getResources() {
+        return resources;
+    }
+
+    public void setResources(List<String> resources) {
+        this.resources = resources;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthLoginConfig{" +
+                "loginPage='" + loginPage + '\'' +
+                ", formRest='" + formRest + '\'' +
+                ", antMatchers=" + antMatchers +
+                ", resources=" + resources +
+                '}';
+    }
+}
